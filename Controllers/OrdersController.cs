d84d3e45ec62477b06ad34bf382f4b8481770a56
Iury Chagas <iurychagas@icloud.com>
2020-03-06 12:56:34 -0300

    add initial controllers

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Event_Hub_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+
+    }
+}
